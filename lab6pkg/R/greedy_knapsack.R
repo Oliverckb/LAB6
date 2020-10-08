0187449b8af59cca7808ca6499fd8716d8372e03
KANGBO CHEN <oliver_chenkb@qq.com>
2020-10-08 14:42:39 +0200

    first version

---
+++ lab6pkg/R/greedy_knapsack.R
@@ -0,0 +1,54 @@
+# Hello, world!
+#
+# This is an example function named 'hello'
+# which prints 'Hello, world!'.
+#
+# You can learn more about package authoring with RStudio at:
+#
+#   http://r-pkgs.had.co.nz/
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Install Package:           'Ctrl + Shift + B'
+#   Check Package:             'Ctrl + Shift + E'
+#   Test Package:              'Ctrl + Shift + T'
+
+
+
+greedy_knapsack <- function(x,W){
+  x$id <- 1:length(x$w)     #give the id to each line
+  df1 <- x[which(x$w < W),]  #find the items that can be taken into the knapsack
+  df1$ratio <- df1$v/df1$w   #calculate the ratio v/w
+  decr_ord <- order(df1$ratio,decreasing = TRUE) #order the items
+
+  df1 <- df1[decr_ord,]  #pick up the those items
+  # print(df1)
+  weight <- 0
+  value <- 0
+  elements <- NULL
+  i <- 1
+  while(weight+df1$w[i]<W && i<length(df1$w)){
+    value <- value + df1$v[i]   #add the value
+    weight <- weight + df1$w[i]  #add the weight of items
+    elements <- c(elements,df1$id[i])  #put it in the elements
+    i <- i+1
+  }
+
+  # print(elements)
+  l <- list(value=value,elements=elements)
+}
+
+####just for test
+set.seed(42)
+n <- 2000
+knapsack_objects <-data.frame(w=sample(1:4000, size = n, replace = TRUE),
+    v=runif(n = n, 0, 10000)
+  )
+
+l1 <- greedy_knapsack(x = knapsack_objects[1:800,], W = 3500)
+l1
+
+
+
+
+
