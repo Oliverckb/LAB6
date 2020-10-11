19de2ad82526c160a21cf95824d3cb5b46a08f4e
Aqsa Iftikhar <aqsaciit@gmail.com>
2020-10-12 00:22:41 +0200

    random

--- R/brute_force_knapsack.R
+++ R/brute_force_knapsack.R
@@ -29,7 +29,7 @@
 #  RNGversion(min(as.character(getRversion()),"4.0.2"))
 #  set.seed(42,kind="Mersenne-Twister",normal.kind = "Inversion")
 #  n <- 2000
-# knapsack_objects <-data.frame(w=sample(1:4000, size = n, replace = TRUE),
+#  knapsack_objects <-data.frame(w=sample(1:4000, size = n, replace = TRUE),
 #                               v=runif(n = n, 0, 10000))
 
 brute_force_knapsack <- function(x, W, parallel=FALSE)
