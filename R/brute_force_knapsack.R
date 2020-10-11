f9b8a0fadd4cb7e6f564e92e5a26afc0ba8bcf84
KANGBO CHEN <oliver_chenkb@qq.com>
2020-10-11 22:30:23 +0200

    ddd

--- R/brute_force_knapsack.R
+++ R/brute_force_knapsack.R
@@ -24,10 +24,10 @@
 #' @importFrom utils combn
 #' @export
  
-# # set.seed(42)
-# # # RNGversion(min(as.character(getRversion()),"4.0.2"))
-# # # set.seed(42,kind="Mersenne-Twister",normal.kind = "Inversion")
-# # n <- 2000
+#  set.seed(42)
+#  RNGversion(min(as.character(getRversion()),"4.0.2"))
+#  set.seed(42,kind="Mersenne-Twister",normal.kind = "Inversion")
+#  n <- 2000
 # knapsack_objects <-data.frame(w=sample(1:4000, size = n, replace = TRUE),
 #                               v=runif(n = n, 0, 10000))
 
