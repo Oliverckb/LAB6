c0992bfbced8eb00bf7dee36995dbd75d14c3a28
Aqsa Iftikhar <70749498+AqsaIftikhar25@users.noreply.github.com>
2020-10-11 09:26:05 +0200

    Update knapsack_brute_force.R

--- R/knapsack_brute_force.R
+++ R/knapsack_brute_force.R
@@ -22,37 +22,53 @@
 #' @seealso  \url{https://en.wikipedia.org/wiki/Knapsack_problem#0.2F1_knapsack_problem}
 #' @export
  
- 
-# set.seed(42)
-# n <- 20
-# knapsack_objects <- data.frame(w=sample(1:40, size = n, replace = TRUE), v=runif(n = n, 0, 10))
+set.seed(42)
+RNGversion(min(as.character(getRversion()),"4.0.2"))
+set.seed(42,kind="Mersenne-Twister",normal.kind = "Inversion")
+n <- 2000
+knapsack_objects <-data.frame(w=sample(1:4000, size = n, replace = TRUE),
+                              v=runif(n = n, 0, 10000)
+)
 
-
-knapsack_brute_force <- function(x,W){
-  
-  item <- NULL
-  value <- NULL
-  weight <- NULL
-  for(i in 1:nrow(x)){
-    item <- c(item,combn(1:nrow(x), i,paste,collapse = ","))
-    value <- c(value,combn(x$v,i,sum))
-    weight <- c(weight,combn(x$w,i,sum))
-    total <- data.frame(item=item,value=value,weight=weight)
+knapsack_BruteForce <- function(x, W)
+{
+  stopifnot(is.data.frame(x) || is.numeric(n) || W >= 0 )
+  if(parallel == FALSE)
+  {
+    object <- NULL
+    weight <- NULL
+    value <- NULL
+    elements <- c()
+    
+    for(i in 1:length(x$w)){
+      object <- c(object,combn(rownames(x), i,paste,collapse = ","))
+      weight <- c(weight,combn(x$w,i,sum))
+      value <- c(value,combn(x$v,i,sum))
+      total <- data.frame(object=object, weight=weight, value=value)
+    }
   }
-  # print(total)
-  # values_col <- colSums(value)
-  # weight_col <- colSums(weight)
   
-  weight_ava <- which(total$weight < W)
-  max_val <- max(total$value[weight_ava])
-  val_id <- total$item[which(total$value == max_val)]
-  make_vector <- as.numeric(unlist(strsplit(val_id,",",fixed=TRUE)))
-  # for(i in 1:length(w_list)){
-  #   for(j in 1:length(w_list[[i]][1,])){
-  #     w_list[[i]][1,]
-  #   }
-  # }
-  #
-  l <- list("value"=max_val,"elements"=make_vector)
-  return(l)
+  else if (parallel == TRUE)
+  {
+    library(parallel)
+    numofCores <- detectCores()-2
+    cl <- makeCluster(numofCores)
+    clusterExport(cl, c('x'), envir = environment())
+    clusterEvalQ(cl , {require(parallel)})
+    object <- parLapply(cl, 1:length(x$w), function(t,f) {combn(rownames(x),t,paste,collapse = ",")}, f=my_fun)
+    weight <- parLapply(cl, 1:length(x$w), function(t,f) {combn(x$w,t,sum)}, f=my_fun)
+    value  <- parLapply(cl, 1:length(x$v), function(t,f) {combn(x$v,t,sum)}, f=my_fun)
+    stopCluster(cl)
+  }
+
+optimal_weights <- which(total$weight<=W)
+max_value <- max(total$value[optimal_weights])
+
+index <- which(total$value == max_value)
+y <- total$object[index]
+
+elements <- as.numeric(unlist(strsplit(y,",")))
+
+knapsack <- list(value = round(max_value),elements= elements)
+return(knapsack)
 }
